d482467dbfab8115363b89080f9b52bd27890dbe
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-01-06 16:46:53 -0800

    Simplify run for the time being, don't need Fire (yet).

--- raindroppy/cli/run.py
+++ raindroppy/cli/run.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """Entry point for primary CLI interaction.."""
-import fire
+# import fire
 from cli.models import CLI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 if __name__ == "__main__":
-    fire.Fire(CLI, name="RaindropPY CLI")
+    CLI()
+    # fire.Fire(CLI, name="RaindropPY CLI")

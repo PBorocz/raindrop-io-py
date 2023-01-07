e8a42cbe99477ebe13875f933e8f2f43b67f236d
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-01-07 12:47:21 -0800

    Consolidated "status", "collections" and "tags" into single main cmd.

---
+++ raindroppy/cli/commands/show.py
@@ -0,0 +1,70 @@
+"""Add/Create a new file-based bookmark to Raindrop."""
+from datetime import datetime
+
+from api.models import Collection, CollectionRef
+from beaupy import select
+from humanize import naturaltime
+from run import CLI  # Typing
+
+CURSOR_STYLE = "red"
+
+
+def _show_status(cli: CLI) -> None:
+    """UI Controller for displaying current status."""
+    human_diff = naturaltime(datetime.utcnow() - cli.state.refreshed)
+    cli.console.print(f"Active User : {cli.state.user.fullName}")
+    cli.console.print(f"Collections : {len(cli.state.collections):3d}")
+    cli.console.print(f"Tags        : {len(cli.state.tags):3d}")
+    cli.console.print(f"As Of       : {human_diff}")
+
+
+def _show_collections(cli: CLI) -> None:
+    """UI Controller for displaying current collections."""
+    # FIXME: Add counts obo Unsorted Collection!
+
+    def get_longest(collections: list[Collection, CollectionRef]) -> int:
+        return max([len(collection.title) for collection in collections if collection.title])
+
+    def get_total_raindrops(collections: list[Collection, CollectionRef]) -> int:
+        return sum([collection.count for collection in collections if collection.count])
+
+    max_len = get_longest(cli.state.collections) + 1
+    total = get_total_raindrops(cli.state.collections)
+
+    cli.console.print(f"{'-'*max_len:{max_len}s} ---")
+    for collection in cli.state.collections:
+        cli.console.print(f"{collection.title:{max_len}s} {collection.count}")
+    cli.console.print(f"{'-'*max_len:{max_len}s} ---")
+    cli.console.print(f"{'Total':{max_len}s} {total:,d}")
+
+
+def _show_tags(cli: CLI) -> None:
+    """UI Controller for displaying current tags."""
+
+    def get_longest(tags: list[str]) -> int:
+        return max([len(tag) for tag in tags])
+
+    max_len = get_longest(cli.state.tags) + 1
+
+    total = 0
+    cli.console.print(f"{'-'*max_len:{max_len}s} ---")
+    for tag in cli.state.tags:
+        cli.console.print(f"{tag:{max_len}s}")
+        total += 1
+    cli.console.print(f"{'-'*max_len:{max_len}s} ---")
+    cli.console.print(f"{'Total':{max_len}s} {total:,d}")
+
+
+def do_show(cli: CLI) -> None:
+    """Top-level UI Controller for showing a set of statistics."""
+    while True:
+        options = ["Connection", "Collections", "Tags", "(done)"]
+        response = select(options)
+        if "done" in response:
+            return None
+        elif response == options[0]:
+            _show_status(cli)
+        elif response == options[1]:
+            _show_collections(cli)
+        elif response == options[2]:
+            _show_tags(cli)
